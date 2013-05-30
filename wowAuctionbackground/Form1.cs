e5fc8578e1f02fbbebf0a22afc89905ddd623092
huangxinxing <hxx0215@126.com>
2013-05-30 15:27:10 +0800

    create thread

--- wowAuctionbackground/Form1.cs
+++ wowAuctionbackground/Form1.cs
@@ -6,14 +6,39 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using System.Threading;
 namespace wowAuctionbackground
 {
     public partial class Form1 : Form
     {
+        private Thread thread = new Thread(new ThreadStart(start));
+        static public bool stop;
+        static public void start()
+        {
+            while (true)
+            {
+                if (!stop)
+                    Console.WriteLine("1");
+                else Thread.Sleep(1000);
+            }
+        }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //thread.Start();
+            //Console.WriteLine("{0}", thread.ThreadState);
+            if (thread.ThreadState==ThreadState.Unstarted)
+                thread.Start();
+            stop = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stop = true;
         }
     }
 }
