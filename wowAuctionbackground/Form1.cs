1376362d943d63d0f5ed9ae6bc0f85bea64b5c74
huangxinxing <hxx0215@126.com>
2013-06-02 01:06:03 +0800

    成功获取单个服务器的联盟拍卖行

--- wowAuctionbackground/Form1.cs
+++ wowAuctionbackground/Form1.cs
@@ -11,6 +11,7 @@ using System.Net;
 using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Timers;
 namespace wowAuctionbackground
 {
     public partial class Form1 : Form
@@ -21,6 +22,8 @@ namespace wowAuctionbackground
         static public string AuctionURL = "/api/wow/auction/data/";
         static public string realmlistURL = "/api/wow/realm/status";
         static public string realm = "激流之傲";
+        static public System.Timers.Timer urltimer = new System.Timers.Timer(1);
+        static public System.Timers.Timer auctiontimer = new System.Timers.Timer(1);
         static public void threadfunc()
         {
             while (true)
@@ -70,10 +73,69 @@ namespace wowAuctionbackground
         {
             string json = HttpGet(Host + realmlistURL, "");
             JObject jo = (JObject)JsonConvert.DeserializeObject(json);
-            
-            List<string> columnname=new List<string>();
             mySQLHelper.open();
-            mySQLHelper.update((JArray)jo["realms"], columnname, "t_realmstatus","name");
+            int num=mySQLHelper.update((JArray)jo["realms"],  "t_realmstatus","name");
+            Console.WriteLine("获取服务器列表成功一共获取{0}个服务器", num);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            urltimer.AutoReset = false;
+            urltimer.Elapsed += new System.Timers.ElapsedEventHandler(getURL);
+            urltimer.Enabled = true;
+        }
+        public void getURL(object source, System.Timers.ElapsedEventArgs e)
+        {
+            string tablename = "t_realmstatus";
+            List<string> columnname = new List<string>();
+            columnname.Add("ID"); columnname.Add("name");
+            List<Dictionary<string, string>> realms = mySQLHelper.getAllData(tablename, columnname);
+            foreach (Dictionary<string, string> dc in realms)
+            {
+                string URL = Host + AuctionURL + dc["name"];
+                string json = HttpGet(URL, "");
+                JObject jo = (JObject)JsonConvert.DeserializeObject(json);
+                ((JObject)jo["files"][0]).Add("FK", dc["ID"]);
+                ((JObject)jo["files"][0]).Add("name", dc["name"]);
+                mySQLHelper.update((JArray)jo["files"], "t_auctionurl", "name");
+            }
+            Console.WriteLine("拉取数据成功一共获取{0}个服务器AH数据", realms.Count);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //auctiontimer.AutoReset = false;
+            //auctiontimer.Elapsed+=new ElapsedEventHandler(getRealTimeAuctionData);
+            //auctiontimer.Enabled = true;
+            getRealTimeAuctionData();
+        }
+        public void getRealTimeAuctionData()//(object source, System.Timers.ElapsedEventArgs e)
+        {
+            string URL = Host + AuctionURL + realm;
+            string json = HttpGet(URL, "");
+            JObject jo = (JObject)JsonConvert.DeserializeObject(json);
+            string AuctionDataURL = jo["files"][0]["url"].ToString();
+            string LastModified = jo["files"][0]["lastModified"].ToString();
+            List<Dictionary<string, string>> ldc = new List<Dictionary<string, string>>();
+            List<string> column=new List<string>();
+            column.Add("lastModified");
+            Dictionary<string, string> clause = new Dictionary<string, string>();
+            clause.Add("chnname", realm); clause.Add("lastModified", LastModified);
+            ldc = mySQLHelper.getData("t_realtimeauctiondata", column,clause);
+            if (ldc.Count > 0)
+            {
+                Console.WriteLine("当前{0}数据已为最新不需要更新", realm);
+                return;//改为continue;
+            }
+            json = HttpGet(AuctionDataURL, "");
+            jo = (JObject)JsonConvert.DeserializeObject(json);
+            string addkey = mySQLHelper.getKeys((JObject)jo["realm"], "");
+            addkey += "`side`,";
+            string addValue = mySQLHelper.getValues((JObject)jo["realm"]);
+            addValue += "'alliance',";
+            string addwhere = "and `name`='" + jo["realm"]["name"] + "'";
+            int num=mySQLHelper.update((JArray)jo["alliance"]["auctions"], "t_realtimeauctiondata", "auc",addkey,addValue,addwhere);
+            Console.WriteLine("共取得{0}联盟拍卖行数据{1}条", realm, num);
         }
     }
 }
--- wowAuctionbackground/mySQLHelper.cs
+++ wowAuctionbackground/mySQLHelper.cs
@@ -10,28 +10,38 @@ namespace wowAuctionbackground
 {
     class mySQLHelper
     {
-        static private MySqlConnection con;
         static private string constr = "database=wowauction;password=922033;User ID=pma;server=192.168.1.102";
         static public void open()
         {
-            con = new MySqlConnection(constr);
+            //con = new MySqlConnection(constr);
             //con.Open();
         }
-        static public void update(JArray jo,List<string> columnname,string tablename,string wherecolumn)
+
+        static public int update(JArray jo, string tablename, string wherecolumn, string addkeys = "", string addvalues = "", string addwhere = "")
         {
+            List<string> w = new List<string>();
+            w.Add(wherecolumn);
+            return update(jo, tablename, w, addkeys, addvalues, addwhere);
+        }
+        static public int update(JArray jo,string tablename,List<string> wherecolumn,string addkeys="",string addvalues="",string addwhere="")
+        {
+            MySqlConnection conn = new MySqlConnection(constr);
             for (int i = 0; i < jo.Count; i++)
             {
-                con.Open();
-                string cmdstr = "select * from " + tablename + " where " + wherecolumn + "='" + jo[i][wherecolumn] + "'";
-                MySqlCommand cmd = new MySqlCommand(cmdstr, con);
+                conn.Open();
+                string where = constractWhereClausewithJObject(wherecolumn, (JObject)jo[i])+addwhere; 
+                string cmdstr = "select * from " + tablename + where; //" where " + wherecolumn + "='" + jo[i][wherecolumn] + "'";
+                MySqlCommand cmd = new MySqlCommand(cmdstr, conn);
                 MySqlDataReader dr = cmd.ExecuteReader();
                 if (dr.Read())
                 {
                     cmdstr = "update "+tablename+" set ";
-                    string key = getKeys((JObject)jo[i], "");
+                    string key = addkeys;
+                    key += getKeys((JObject)jo[i], "");
                     key = key.Remove(key.Length - 1, 1);
                     string[] keys=key.Split(new Char[] { ',' });
-                    string value = getValues((JObject)jo[i]);
+                    string value = addvalues;
+                    value += getValues((JObject)jo[i]);
                     value = value.Remove(value.Length - 1, 1);
                     string[] values = value.Split(new Char[] { ',' });
                     if (keys.Length != values.Length)
@@ -41,11 +51,11 @@ namespace wowAuctionbackground
                         for (int j = 0; j < keys.Length; j++)
                             cmdstr += " " + keys[j] + " = " + values[j] + " , ";
                         cmdstr = cmdstr.Remove(cmdstr.Length - 2, 2);
-                        cmdstr += " where " + wherecolumn + "='" + jo[i][wherecolumn] + "'";
+                        cmdstr += where;
                         MySqlConnection connect = new MySqlConnection(constr);
                         connect.Open();
                         MySqlCommand upcmd = new MySqlCommand(cmdstr, connect);
-                        Console.Write("update {0}...", jo[i][wherecolumn]);
+                        Console.Write("update {0} to {1}...", where,tablename);
                         upcmd.ExecuteNonQuery();
                         Console.WriteLine("Success!");
                         connect.Close();
@@ -53,22 +63,25 @@ namespace wowAuctionbackground
                 }
                 else
                 {
-                    string key= getKeys((JObject)jo[i],"");
+                    string key = addkeys;
+                    key+= getKeys((JObject)jo[i],"");
                     key = key.Remove(key.Length - 1, 1);
-                    string value = getValues((JObject)jo[i]);
+                    string value = addvalues;
+                    value += getValues((JObject)jo[i]);
                     value = value.Remove(value.Length - 1, 1);
                     cmdstr = "insert into " + tablename + " (" + key + ") values (" + value + ")";
                     MySqlConnection connect = new MySqlConnection(constr);
                     connect.Open();
                     MySqlCommand inscmd = new MySqlCommand(cmdstr, connect);
-                    Console.Write("insert {0}...", jo[i][wherecolumn]);
+                    Console.Write("insert {0} into {1}...", where,tablename);
                     inscmd.ExecuteNonQuery();
                     Console.WriteLine("Success!");
                     connect.Close();
                 }
-                con.Close();
+                conn.Close();
             }
-            Console.WriteLine("获取服务器列表成功一共获取{0}个服务器", jo.Count);
+            return jo.Count;
+            //Console.WriteLine("获取服务器列表成功一共获取{0}个服务器", jo.Count);
         }
         static public string getKeys(JObject jo,string prekey)
         {
@@ -109,6 +122,57 @@ namespace wowAuctionbackground
                 else ret += "'"+dc[key].ToString()+"'"+",";
             }
             return ret;
+        }
+        static public List<Dictionary<string, string>> getAllData(string tablename, List<string> columnname)
+        {
+            //Dictionary<string, string> c = new Dictionary<string, string>();
+            return getData(tablename, columnname, "");
+        }
+        static public List<Dictionary<string, string>> getData(string tablename, List<string> columnname, Dictionary<string, string> clause)
+        {
+            string c=constractWhereClause(clause);
+            return getData(tablename, columnname, c);
+        }
+        static public List<Dictionary<string, string>> getData(string tablename, List<string> columnname,string clause)
+        {
+            List<Dictionary<string, string>> ret = new List<Dictionary<string, string>>();
+            MySqlConnection conn = new MySqlConnection(constr);
+            conn.Open();
+            string columns = "";
+            foreach (string str in columnname) columns += "`"+str+"`" + ",";
+            columns = columns.Remove(columns.Length - 1, 1);
+            string cmdstr = "select " + columns + " from " + tablename + " "+clause;
+            MySqlCommand cmd = new MySqlCommand(cmdstr, conn);
+            MySqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Dictionary<string, string> dc = new Dictionary<string, string>();
+                foreach (string str in columnname)
+                    dc.Add(str, dr[str].ToString());
+                ret.Add(dc);
+            }
+            conn.Close();
+            return ret;
+        }
+        static public string constractWhereClause(Dictionary<string, string> clause)
+        {
+            string ret = " where ";
+            foreach (string key in clause.Keys)
+            {
+                ret += "`"+key +"`"+ " = '" + clause[key] + "' and ";
+            }
+            if (ret.Length > 7)
+                ret = ret.Remove(ret.Length - 4, 4);
+            else
+                ret = "";
+            return ret;
+        }
+        static public string constractWhereClausewithJObject(List<string> wherecolumn, JObject jo)
+        {
+            Dictionary<string, string> c = new Dictionary<string, string>();
+            foreach (string str in wherecolumn)
+                c.Add(str, jo[str].ToString());
+            return constractWhereClause(c);
         }
     }
 }
